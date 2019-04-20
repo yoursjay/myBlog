b03605bf37956e297d6afa1ac580b3d207e7c35f
hey <120438436@qq.com>
2019-04-20 17:05:08 +0800

    完善了Gallery这个APP
    主要增加了标题（tittle）和图片（image）
    该版本网页未优化，只能单纯的看到文字和图片，并没有对HTML文件进行排版

--- .history/gallery/models_20190420163944.py
+++ .history/gallery/models_20190420163944.py
@@ -6,7 +6,7 @@ from django.db import models
 class Gallery(models.Model):
     description = models.CharField(default='在这里输入作品的简介',max_length=100)
     tittle = models.CharField(default='作品标题',max_length=100)
-    image = models.ImageField(default='default.png',upload_to='images/')
+    image = models.ImageField(default='default.png',upload_to='E:/images/')
     
     def __str__(self):
         """Unicode representation of MODELNAME."""
